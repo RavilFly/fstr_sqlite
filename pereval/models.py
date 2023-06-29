358ed385c4a64687c8fa7d51b576494c5bf0717b
Ravil.Flyday <rmm68@mail.ru>
2023-06-29 16:19:53 +0300

    Init project, startapp, add models

---
+++ pereval/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+
+# Create your models here.
+
+class Coords(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    height = models.IntegerField()
+
+    class Meta:
+        db_table = 'coords'
+
+class PerevalAdded(models.Model):
+    STATUS_CHOICE = [
+        ('new', 'Новый'),
+        ('pending', 'На рассмотрении'),
+        ('accepted', 'Принят'),
+        ('rejected', 'Отклонен'),
+    ]
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    add_time = models.TimeField(auto_now_add=True)
+    raw_data = models.JSONField()
+    status = models.CharField(max_length=15, choices=STATUS_CHOICE, default='new')
+    beauty_title = models.TextField(blank=True, null=True)
+    title = models.TextField(blank=True, null=True)
+    other_titles = models.TextField(blank=True, null=True)
+    connect = models.TextField(blank=True, null=True)
+    winter = models.CharField(max_length=5, default='')
+    spring = models.CharField(max_length=5, default='')
+    summer = models.CharField(max_length=5, default='')
+    autumn = models.CharField(max_length=5, default='')
+    coords = models.ForeignKey('Coords', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'pereval_added'
+
+class PerevalAreas(models.Model):
+    id_parent = models.IntegerField()
+    title = models.CharField(max_length=255, blank=True, null=True)
+
+    class Meta:
+        db_table = 'pereval_areas'
+
+# class SprActivitiesTypes(models.Model):
+#     title = models.CharField(max_length=255)
+#
+#     class Meta:
+#         db_table = 'spr_activities_types'
+
+class Images(models.Model):
+    date_added = models.DateTimeField(auto_now_add=True)
+    img = models.BinaryField()
+
+    class Meta:
+        db_table = 'images'
+
+class PerevalImages(models.Model):
+    pereval = models.ForeignKey('PerevalAdded', on_delete=models.CASCADE)
+    image = models.ForeignKey('Images', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'pereval_images'
+
+class Users(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    mid_name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.CharField(max_length=255, unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+
+    class Meta:
+        db_table = 'users'
+
